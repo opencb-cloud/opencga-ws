1eaac279d3b4cfc777568300906c9c4f7ed4a6bc
echirivella <echirivella@gmail.com>
2012-11-08 16:07:29 +0100

    Test Changes

--- src/main/test/org/bioinfo/gcs/lib/feature/FeatureManagerTest.java
+++ src/main/test/org/bioinfo/gcs/lib/feature/FeatureManagerTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 public class FeatureManagerTest {
 
 	@Test
-	public void test() {
+	public void testBed() {
 		FeatureManager fm = new FeatureManager("/home/echirivella/Downloads/testbed/","Homo.bed.gz");
 		try {
 			
@@ -18,5 +18,15 @@ public class FeatureManagerTest {
 			e.printStackTrace();
 		}
 	}
-
+	
+	@Test
+	public void testGff() {
+		FeatureManager fm = new FeatureManager("/home/echirivella/Downloads/testbed/","Homo.bed.gz");
+		try {
+			
+			System.out.println(fm.getByRegion("Homo.bed.gz","chr10", 80000, 100000));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
