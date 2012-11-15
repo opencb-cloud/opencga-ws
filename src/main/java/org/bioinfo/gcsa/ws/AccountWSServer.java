c63fdd1ebc8114bb975f50c45cdaa647824a8ad6
echirivella <echirivella@gmail.com>
2012-11-15 10:57:19 +0100

    pom.xml: some versions upgraded

--- src/main/java/org/bioinfo/gcsa/ws/AccountWSServer.java
+++ src/main/java/org/bioinfo/gcsa/ws/AccountWSServer.java
@@ -9,26 +9,30 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
+import org.bioinfo.gcsa.lib.users.CloudSessionManager;
 import org.bioinfo.gcsa.lib.users.persistence.UserManagementException;
-import org.bioinfo.gcsa.lib.users.persistence.UserMongoDBManager;
+import org.bioinfo.gcsa.lib.users.persistence.UserManager;
 
 
 
 @Path("/account")
 public class AccountWSServer extends GenericWSServer  {
-	private UserMongoDBManager userManager= new UserMongoDBManager();
-	
-	public AccountWSServer(@Context UriInfo uriInfo) throws IOException {
+	private UserManager userManager;
+	private CloudSessionManager cloudSessionManager = null;
+	public AccountWSServer(@Context UriInfo uriInfo) throws IOException, UserManagementException {
 		super(uriInfo);
+		System.err.println("----------------------------------->");
+		cloudSessionManager = new CloudSessionManager("GCSA_HOME");
+		userManager = CloudSessionManager.userManager;
 	}
 	
 	@GET
 	@Path("/register/{accountId}/{password}/{accountName}/{email}")
 	public Response register(@PathParam("accountId") String accountId,@PathParam("password") String password,@PathParam("accountName") String accountName, @PathParam("email") String email){
 		try {
-			userManager.createAccountId(accountId,password,accountName,email);
+			userManager.createUser(accountId,password,accountName,email);
 		} catch (UserManagementException e) {
-			e.printStackTrace();
+			return createErrorResponse(e.toString());
 		}
 		return createOkResponse("OK");
 	}
