34cb56b293017170eb4b9798bce6775249978b56
echirivella <echirivella@gmail.com>
2012-11-15 15:07:36 +0100

    AccountWSServer: Change to cath the IP

--- src/main/java/org/bioinfo/gcsa/ws/AccountWSServer.java
+++ src/main/java/org/bioinfo/gcsa/ws/AccountWSServer.java
@@ -2,6 +2,7 @@ package org.bioinfo.gcsa.ws;
 
 import java.io.IOException;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -21,6 +22,7 @@ public class AccountWSServer extends GenericWSServer  {
 	private CloudSessionManager cloudSessionManager = null;
 	public AccountWSServer(@Context UriInfo uriInfo) throws IOException, UserManagementException {
 		super(uriInfo);
+		
 		System.err.println("----------------------------------->");
 		cloudSessionManager = new CloudSessionManager("GCSA_HOME");
 		userManager = CloudSessionManager.userManager;
@@ -28,8 +30,15 @@ public class AccountWSServer extends GenericWSServer  {
 	
 	@GET
 	@Path("/register/{accountId}/{password}/{accountName}/{email}")
-	public Response register(@PathParam("accountId") String accountId,@PathParam("password") String password,@PathParam("accountName") String accountName, @PathParam("email") String email){
+	public Response register(@Context HttpServletRequest httpServletRequest,@PathParam("accountId") String accountId,@PathParam("password") String password,@PathParam("accountName") String accountName, @PathParam("email") String email){
 		try {
+			
+			//Pruebas ip
+			String IPaddr = httpServletRequest.getRemoteAddr().toString();
+			String IPhost = httpServletRequest.getRemoteHost().toString();
+			System.out.println("--------------- > la ip es:" + IPaddr);
+			System.out.println("--------------- > la ip es:" + IPhost);
+			//Fin pruebas ip
 			userManager.createUser(accountId,password,accountName,email);
 		} catch (UserManagementException e) {
 			return createErrorResponse(e.toString());
