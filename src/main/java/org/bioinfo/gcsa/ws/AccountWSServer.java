a9b03f477c346b27577d3b8509fcb4cfa8bec801
echirivella <echirivella@gmail.com>
2012-11-22 12:40:36 +0100

    users: correct url

--- src/main/java/org/bioinfo/gcsa/ws/AccountWSServer.java
+++ src/main/java/org/bioinfo/gcsa/ws/AccountWSServer.java
@@ -1,20 +1,16 @@
 package org.bioinfo.gcsa.ws;
 
 import java.io.IOException;
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
-import org.bioinfo.gcsa.lib.GcsaUtils;
 import org.bioinfo.gcsa.lib.users.CloudSessionManager;
 import org.bioinfo.gcsa.lib.users.beans.Session;
 import org.bioinfo.gcsa.lib.users.persistence.UserManagementException;
@@ -36,8 +32,8 @@ public class AccountWSServer extends GenericWSServer  {
 	}
 	
 	@GET
-	@Path("/register/{accountId}/{password}/{accountName}/{email}")
-	public Response register(@PathParam("accountId") String accountId,@PathParam("password") String password,@PathParam("accountName") String accountName, @PathParam("email") String email){
+	@Path("/{accountId}/create")
+	public Response register(@PathParam("accountId") String accountId,@QueryParam("password") String password,@QueryParam("accountName") String accountName, @QueryParam("email") String email){
 
 		Session session = new Session(sessionIp);
 		
@@ -50,8 +46,8 @@ public class AccountWSServer extends GenericWSServer  {
 	}
 
 	@GET
-	@Path("/login/{accountId}/{password}")
-	public Response login(@PathParam("accountId") String accountId,@PathParam("password") String password){
+	@Path("/{accountId}/login")
+	public Response login(@PathParam("accountId") String accountId,@QueryParam("password") String password){
 		Session session = new Session(sessionIp);
 		return createOkResponse(userManager.login(accountId, password, session));
 	}
@@ -62,6 +58,11 @@ public class AccountWSServer extends GenericWSServer  {
 		return createOkResponse(userManager.testPipe(accountId, password));
 	}
 
+	@GET
+	@Path("/{accountId}/logout") 
+	public Response logout(@PathParam("accountId") String accountId,@QueryParam("sessionId") String sessionId){
+		return createOkResponse(createOkResponse(userManager.logout(accountId, sessionId)));
+	}
 	
 	
 //	@GET
