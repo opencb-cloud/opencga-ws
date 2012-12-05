b00d273ed1cf108e62cc198fae64d66b22dee800
Francisco Salavert Torres <fsalavert@cipf.es>
2012-12-05 10:41:51 +0100

    CloudSessionManager is static instantiated

---
+++ src/main/test/org/bioinfo/gcsa/lib/feature/FeatureManagerTest.java
@@ -0,0 +1,33 @@
+package org.bioinfo.gcsa.lib.feature;
+
+import java.io.IOException;
+
+import org.bioinfo.gcsa.lib.storage.feature.FeatureManager;
+import org.junit.Test;
+
+
+
+public class FeatureManagerTest {
+
+	@Test
+	public void testBed() {
+		FeatureManager fm = new FeatureManager("/home/echirivella/Downloads/testbed/","Homo.bed.gz");
+		try {
+			
+			System.out.println(fm.getByRegion("Homo.bed.gz","chr10", 80000, 100000));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testGff() {
+		FeatureManager fm = new FeatureManager("/home/echirivella/Downloads/testbed/","Homo.bed.gz");
+		try {
+			
+			System.out.println(fm.getByRegion("Homo.bed.gz","chr10", 80000, 100000));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
