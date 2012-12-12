0c6245abd5c4956147985ca3cd3460456d74cb1c
Ruben Sanchez <rsanchez@cipf.es>
2012-12-12 12:06:01 +0100

    Analysis: changes in getDataPath

--- src/main/java/org/bioinfo/gcsa/ws/AnalysisWSServer.java
+++ src/main/java/org/bioinfo/gcsa/ws/AnalysisWSServer.java
@@ -122,9 +122,6 @@ public class AnalysisWSServer extends GenericWSServer {
 	}
 
 	private Response analysis(String analysisStr, MultivaluedMap<String, String> params) {
-		// System.out.println("params: "+params.toString());
-		// Map<String, List<String>> paramsMap = params;
-
 		// TODO Comprobar mas cosas antes de crear el analysis job executer
 		// (permisos, etc..)
 		
@@ -200,7 +197,7 @@ public class AnalysisWSServer extends GenericWSServer {
 				List<String> dataIds = Arrays.asList(params.get(inputParam.getName()).get(0).split(","));
 				List<String> dataPaths = new ArrayList<String>();
 				for (String dataId : dataIds) {
-					String dataPath = cloudSessionManager.getDataPath(dataId, sessionId);
+					String dataPath = cloudSessionManager.getDataPath(project, dataId, sessionId);
 					if (dataPath.contains("ERROR")) {
 						return createErrorResponse(dataPath);
 					} else {
